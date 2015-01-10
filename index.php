137e8134e26908dd9e2dee1a85e930a4fa4c7f31
j4ck4ll <kabimel@gmail.com>
2015-01-10 11:45:30 -0500

    TO DO: rewrite header/helper functions

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 require_once('./includes/header.php');
 
    $xml = new SimpleXMLElement($xmlstr);
+   echo "<table>";
+   echo "<th>Pizza</th>";
+   echo "<th>Size</th>";
    foreach($xml->pizzas->pizza as $pizza) {
-       echo $pizza->name, '<br>';
+       echo "<tr>";
+       echo "<td>";
+           echo $pizza->name;
+       echo "</td>";
+       echo "<td>";
+        echo $pizza->size;
+       echo "</td";
+       echo "</tr>";
    }
-
+    echo "</table>";
 require_once('./includes/footer.php');
 
 ?>
